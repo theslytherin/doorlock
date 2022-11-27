e8e4599bdd829b479913a00c272340bd17200fa2
Anuska Sinha <anuskasinha786@gmail.com>
2022-11-27 15:42:39 -0500

    Update lock.py

--- lock.py
+++ lock.py
@@ -33,9 +33,9 @@ class Fullscreen_Window:
         global dbPass
         
         dbHost = 'localhost'
-        dbName = 'DB_NAME'
-        dbUser = 'USER'
-        dbPass = 'PASSWORD'
+        dbName = 'door_lock'
+        dbUser = 'anu'
+        dbPass = 'root1'
         
         def __init__(self):
                 self.tk = tk.Tk()
@@ -58,7 +58,7 @@ class Fullscreen_Window:
                 t.start()
         def show_idle1():
             self.welcomeLabel = ttk.Label(self.tk, text= " PLease choose:")
-            
+            self.show_idle(self)
                 
         def show_idle(self):
                     B = Tkinter.Button(top, text ="Resident", command = self.show_idle())
@@ -146,7 +146,7 @@ class Fullscreen_Window:
                                                                 self.validUser = ttk.Label(self.tk, text="Welcome\n %s!" % (user_info['name']), font='size, 15', justify='center', anchor='center')
                                                                 self.validUser.grid(columnspan=3, sticky=tk.W+tk.E)
                                                                 
-                                                                self.image = tk.PhotoImage(file=user_info['image'] + ".gif")
+                                                                self.image = tk.PhotoImage(file="home/root1/Desktop/"+ user_info['image'] + ".gif")
                                                                 self.photoLabel = ttk.Label(self.tk, image=self.image)
                                                                 self.photoLabel.grid(columnspan=3)
                                                                 
